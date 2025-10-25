ae572fc5173a6a566d1748b7d49f9db29d1d73c2
Juveriya Minhaj <juveriya.minhaj.315@gmail.com>
2025-10-25 16:53:56 +0530

    my first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('Myform.html')
+@app.route('/submit',methods=['POST'])
+def submit():
+    fname = request.form['fname']
+    lname=request.form['lname']
+    age = request.form['age']
+    phno = request.form['phno']
+    return render_template('greetings.html',fname = fname,lname = lname,age = age,phno =phno)
+if(__name__ == "__main__"):
+    app.run(debug =True)
